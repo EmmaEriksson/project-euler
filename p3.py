4989e772d11608922a32b954aeeffe263704d082
moalar <moalar@kth.se>
2020-11-21 20:25:02 +0100

    Add p3 solution <3

--- p3.py
+++ p3.py
@@ -1,42 +1,27 @@
-# Största primtalsfaktorn i 600851475143
+# Hitta största primtalsfaktorn i 600851475143
 
-# Gör en lista av primtalen upp till kvadratroten av talet, 
-# testa sedan % av primtalen i listan.
-# Hitta primtalen genom att testa om varje tal (starta på 2) 
-# är delbart med primtalen innan på listan upp till sqrt av talet som ska testas.
-# Om det inte är delbart med någon av de är det ett primtal och läggs till i 
-# listan av primtal.
-
-import math
 from math import sqrt, ceil
 
 talet = 600851475143
 rot = sqrt(talet)
-primtal = [3, 5, 7, 11, 13, 17] # inkl inte 2 pga testar bara udda tal
-
-#print(rot)
-
-# hitta primtal
-j = 0
-boo = True
+primtal = [3, 5, 7, 11, 13, 17] # exkl 2 pga testar bara udda tal
+primtalsfaktorer = []
 
 # fixa: for tal = [19:2:ceil(rot)] fast i Python
-for tal in range(19, ceil(rot)): # räknar igenom alla tal, startar på 19 upp roten av talet
-    while primtal[j] < sqrt(tal): # testar mot primtalen upp till sqrt(tal)
-        if tal%primtal[j] == 0: # delbart, inte ett primtal
+for tal in range(19, ceil(rot)+1, 2): # räknar igenom alla tal, startar på 19 upp roten av talet
+    boo = True
+    for p in primtal: # testar mot primtalen upp till sqrt(tal)
+        if p > sqrt(tal):
+            break
+        if tal%p == 0: # delbart, inte ett primtal
             boo = False
-        else: 
-            if not boo: # om boo redan är F, fortsätter den vara F
-                boo = False
-            else:
-                boo = True 
-        j += 1 
+            break
+    if boo:
+        primtal.append(tal) # lägger till primtal i listan
+        if talet%tal == 0: # primtalet är en faktor till talet
+            primtalsfaktor = tal # sparar endast den största primtalsfaktorn
+            primtalsfaktorer.append(tal) # kan spara alla i lista också
 
-
-# kontrollera primtalsfaktorer i talet
-i = 0
-while primtal[i] < rot:   
-    if talet%primtal[i] == 0: # primtalet är en faktor till talet
-        primtalsfaktor = primtal[i] # sparar endast den största primtalsfaktorn
-        # kan spara alla i lista också
-    i += 1 # öka index med 1
+print(primtalsfaktor)
+print(primtalsfaktorer)
+print(talet/primtalsfaktor) # kontroll att det är delbart, ok
